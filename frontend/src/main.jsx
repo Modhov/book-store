56cecb455571badce7e42df9c2dcf26f40c69525
Modhov <shrirammadhav2004@gmail.com>
2024-07-23 15:22:58 +0530

    signin created

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,25 +1,11 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Login from "./Login.jsx";
+import { HashRouter } from "react-router-dom";
+import Path from "./Path.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Login />
-  </React.StrictMode>
+  <HashRouter>
+    <Path />
+  </HashRouter>
 );
-=======
-import React from 'react'
-import ReactDOM from 'react-dom/client'
- import './index.css'
-import { HashRouter } from 'react-router-dom';
-import Path from './Path.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <HashRouter>
-      <Path/>
-    </HashRouter> 
-   
-)
->>>>>>> 15a04c1caba95454cd09a99c33d663e671b627d4
